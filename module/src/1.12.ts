ded5a60445db9ab96b323e72ccbed4001dc73b66
Bappy <bappymahmud321@gmail.com>
2023-11-03 18:06:41 +0600

    added

---
+++ module/src/1.12.ts
@@ -0,0 +1,19 @@
+{
+
+// nullable type
+
+const searchName = (value :string) => {
+    if(value) {
+        console.log('seatching');
+    }
+    else{
+        console.log('there is nothing to search');
+    }
+} 
+searchName('Bappy')
+
+
+
+
+
+}
